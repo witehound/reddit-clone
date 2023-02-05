41919278cac307d2bbc9b8dfac9a841b4c05968d
witehound <netvid00@gmail.com>
2023-02-05 20:57:45 +0100

    cleaned up code

--- src/components/Navbar/RightContent/UserMenu/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu/UserMenu.tsx
@@ -6,23 +6,33 @@ import {
   MenuList,
   Icon,
   Flex,
+  Text,
 } from "@chakra-ui/react";
 import { User } from "firebase/auth";
 import { FaRedditSquare } from "react-icons/fa";
 import { VscAccount } from "react-icons/vsc";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogout } from "react-icons/md";
+import { HiSparkles } from "react-icons/hi";
 import { fireBaseAuth } from "../../../../service/index";
 import { signOut } from "firebase/auth";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/src/components/atoms";
 
 type UserMenuType = {
   user: User | null | undefined;
 };
 
 const UserMenu = ({ user }: UserMenuType) => {
+  const setAuthModal = useSetRecoilState(authModalState);
   const signUserOut = () => {
     signOut(fireBaseAuth);
   };
+
+  const openAuthModal = () => {
+    setAuthModal((prev) => ({ open: true, view: "login" }));
+  };
+
   return (
     <div>
       <Menu>
@@ -33,10 +43,10 @@ const UserMenu = ({ user }: UserMenuType) => {
             borderRadius={4}
             _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
           >
-            {user ? (
-              <>
-                <Flex align={"center"}>
-                  <Flex align={"center"} justify={"center"}>
+            <Flex align={"center"}>
+              <Flex align={"center"} justify={"center"}>
+                {user ? (
+                  <>
                     <Icon
                       as={FaRedditSquare}
                       fontSize={22}
@@ -44,19 +54,38 @@ const UserMenu = ({ user }: UserMenuType) => {
                       mr={1}
                       ml={1}
                     />
+
+                    <Flex
+                      direction={"column"}
+                      display={{ base: "none", lg: "flex" }}
+                      fontSize={"8pt"}
+                      align={"flex-end"}
+                      mr={8}
+                    >
+                      <Text>
+                        {user?.displayName || user?.email?.split("@")[0]}
+                      </Text>
+                      <Flex>
+                        <Icon as={HiSparkles} mr={1} color="brand.100" />
+                        <Text color={"gray.400"}>1 karma</Text>
+                      </Flex>
+                    </Flex>
                     <ChevronDownIcon />
-                  </Flex>
-                </Flex>
-              </>
-            ) : (
-              <Icon
-                as={VscAccount}
-                fontSize={22}
-                color={"gray.400"}
-                mr={1}
-                ml={1}
-              />
-            )}
+                  </>
+                ) : (
+                  <>
+                    <Icon
+                      as={VscAccount}
+                      fontSize={22}
+                      color={"gray.400"}
+                      mr={1}
+                      ml={1}
+                    />
+                    <ChevronDownIcon />
+                  </>
+                )}
+              </Flex>
+            </Flex>
           </MenuButton>
         </Flex>
 
@@ -92,10 +121,11 @@ const UserMenu = ({ user }: UserMenuType) => {
                 fontSize={"10pt"}
                 fontWeight={700}
                 _hover={{ bg: "blue.500", color: "white" }}
+                onClick={openAuthModal}
               >
                 <Flex align={"center"}>
                   <Icon as={CgProfile} fontSize={20} mr={2} />
-                  Profile
+                  Log In / Sign Up{" "}
                 </Flex>
               </MenuItem>
             </>
